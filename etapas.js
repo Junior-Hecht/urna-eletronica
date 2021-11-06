b9017457397ad20f4b7c1d9d841ee204ad23ea1d
Werner Hecht Junior <juniorhechtjr@gmail.com>
2021-11-06 12:16:32 -0300

    Arquivos adicionados

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '11111',
+                nome: 'Nanashi-no-Gombei',
+                partido: 'ABC',
+                fotos: [
+                    { url: '66.png', legenda: 'Vereador' }
+                ]
+            },
+            {
+                numero: '22222',
+                nome: 'Beltrano Só',
+                partido: 'DEFG',
+                fotos: [
+                    { url: '66_2.png', legenda: 'Vereador' }
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '84',
+                nome: 'Fulano de Tal',
+                partido: 'HIJ',
+                vice: 'Zulana Política ',
+                fotos: [
+                    { url: '84.png', legenda: 'Prefeito' },
+                    { url: '84_2.png', legenda: 'Vice-Prefeito', small: true }
+                ]
+            },
+            {
+                numero: '51',
+                nome: 'Ciclana Vainafé',
+                partido: 'VXZ',
+                vice: 'Zé Comum',
+                fotos: [
+                    { url: '51_2.png', legenda: 'Prefeito' },
+                    { url: '51.png', legenda: 'Vice-Prefeito', small: true }
+                ]
+            },
+        ]
+    }
+];
